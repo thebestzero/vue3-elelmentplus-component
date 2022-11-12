0a3d70bab2b7442c5f3370d9ff619f417e115eef
thebestzero <zhangbestzero@gmial.com>
2022-11-13 00:11:39 +0800

    test

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,7 @@
     <h1>首页</h1>
     <el-button type="primary">Primary</el-button>
     <el-button type="info">Info</el-button>
+    <el-button type="info">Info</el-button>
 </template>
 
 <script lang="ts">
